1f0730c405f5074011f0371cdf9c011f178410bb
Daniel Hooper <danielchasehooper@gmail.com>
2015-03-13 17:02:41 -0400

    Experiment with using a ABPeoplePickerNavigationController in a UISplitView

--- MyContacts/AppDelegate.swift
+++ MyContacts/AppDelegate.swift
@@ -15,75 +15,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UINavigationControllerDel
 
     var window: UIWindow?
     var addressBook: ABAddressBook!
-    var masterNav: UINavigationController?
+    let splitViewController = UISplitViewController()
+//    var masterNav: UINavigationController?
     
     // MARK: - UIApplicationDelegate
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        self.window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
-        self.masterNav = UINavigationController.init()
-        self.masterNav?.delegate = self
-        self.window?.rootViewController = self.masterNav?
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let peoplePicker = ABPeoplePickerNavigationController()
+        peoplePicker.peoplePickerDelegate = self
+        splitViewController.viewControllers = [peoplePicker]
+        self.window?.rootViewController = splitViewController
         self.window?.makeKeyAndVisible()
-        self.checkAddressBookAccess()
         return true
     }
- 
-    func checkAddressBookAccess() {
-        switch(ABAddressBookGetAuthorizationStatus()) {
-        case .Authorized: self.accessGrantedForAddressBook()
-        case .NotDetermined: self.requestAddressBookAccess()
-        case .Denied: fallthrough
-        case .Restricted:
-            println("Address Book access either Denied or Restricted")
-        }
-    }
     
-    func requestAddressBookAccess() {
-        ABAddressBookRequestAccessWithCompletion(addressBook) {
-            (granted: Bool, err: CFError!) in
-            if(granted) {
-                dispatch_async(dispatch_get_main_queue()) {
-                    self.accessGrantedForAddressBook()
-                }
-            } else {
-                println("ABAddressBookRequestAccessWithCompletion handler got denied!")
-            }
-        }
-    }
-    
-    func accessGrantedForAddressBook() {
-        println("accessGrantedForAddressBook")
-        var err: Unmanaged<CFError>? = nil
-        let ab = ABAddressBookCreateWithOptions(nil, &err)
-        if err == nil {
-            self.addressBook = ab.takeRetainedValue()
-        } else {
-            assertionFailure("Could not create Address Book!")
-            // Tell the user to go to Settings.app -> Privacy -> Contacts -> Enable ABC
-        }
-        
-        showPeoplePickerController()
-    }
-    
-    func showPeoplePickerController() {
-        println("showPeoplePickerController")
-        
-        let picker = ABPeoplePickerNavigationController()
-        picker.peoplePickerDelegate = self
-        
-        // This is nil.... apparently this started in iOS 8
-        //println("ABPeoplePickerNavigationController.topViewController = \(picker.topViewController?)")
-        
-        // This crashes!
-        //self.masterNav?.setViewControllers([picker.topViewController], animated: true)
-        
-        // This the only way I was able to add ABPeoplePicker to my root UINavigationController
-        self.masterNav?.view.addSubview(picker.view)       // This must come before addChildViewController!!!
-        self.masterNav?.addChildViewController(picker)
-        picker.didMoveToParentViewController(self.masterNav?)
-    }
+//    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+//        return true
+//    }
     
     // MARK: - ABPeoplePickerNavigationControllerDelegate
     
@@ -93,16 +44,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UINavigationControllerDel
         personVC.allowsEditing = true
         personVC.allowsActions = true
         personVC.displayedPerson = person
+
+        splitViewController.showDetailViewController(personVC, sender: nil)
         
-        // This appears to do nothing :(
-        self.masterNav?.setNavigationBarHidden(false, animated: false)
-        
-        // This works but there is no navigation bar!? WHY?  Please help me :|
-        self.masterNav?.showViewController(personVC, sender: nil)
-        
-        // Does not work!
-        //self.masterNav?.view.addSubview(personVC.view)
-        //self.masterNav?.addChildViewController(personVC)
+//        splitViewController.viewControllers = [splitViewController.viewControllers[0], personVC]
     }
     
     func peoplePickerNavigationControllerDidCancel(peoplePicker: ABPeoplePickerNavigationController!) {

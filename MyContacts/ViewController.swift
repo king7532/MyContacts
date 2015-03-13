bb48f121288cc0126f3864db1385291483dec624
Benjamin King <king7532@mac.com>
2015-03-12 21:47:48 -0400

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Benjamin King on 3/12/15.
+//  Copyright (c) 2015 Benjamin King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
